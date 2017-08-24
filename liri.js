9b84251fc48630999b3039d9c38434930cb23879
johnnymoctezuma <johnnymocc@gmail.com>
2017-08-23 19:58:44 -0700

    userRequests

--- liri.js
+++ liri.js
@@ -1,3 +1,19 @@
-var fs = require('fs');
+var keys = require('./keys.js');
+// console.log(keys);
 
-process.argv[2];
+var userRequest = process.argv[2];
+
+// console.log('user ', userRequest)
+
+if (userRequest === 'my-tweets'){
+  console.log('mytweets');
+}
+if (userRequest === 'spotify-this-song'){
+  console.log('spotifyThis');
+}
+if (userRequest === 'movie-this'){
+  console.log('movieThis');
+}
+if (userRequest === 'do-what-it-says'){
+  console.log('doThis');
+}
