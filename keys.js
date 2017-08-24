778c3c37c03431d3492b394c62d97b36f2964758
johnnymoctezuma <johnnymocc@gmail.com>
2017-08-23 19:46:30 -0700

    added keys

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'cMhVi8OPGD5URicoHHe5N1vj6',
+  consumer_secret: 'gSA4XltuD7tv0XTUdfHi3Bm3afHwsD9hEuIGd0wk6i8GwhnuIn',
+  access_token_key: '2205707299-CkqCilzcV9AY0N36doYczd3nTYB5kiCeUXy6d6w',
+  access_token_secret: 'vBMp4y7Pmpp5MHgLuLMnYF8VziyHe9fVDMo0RaNbusisU',
+}
---
+++ liri.js
@@ -0,0 +1,3 @@
+var fs = require('fs');
+
+process.argv[2];
